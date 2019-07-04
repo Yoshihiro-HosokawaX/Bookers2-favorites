90c3ebdf52877094f5772f089317c76426673cc5
Yoshihiro-HosokawaX <hosokawa.yoshihiro.infratop@gmail.com>
2019-07-04 21:30:07 +0900

    Bookers2いいね機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,11 @@
 class FavoritesController < ApplicationController
-    
+
+    #いいね機能
     def create
-        book = Book.find(params[:book_id])
-        favorite = current_user.favorites.new(book_id: book.id)
+        @book = Book.find(params[:book_id])
+        favorite = current_user.favorites.new(book_id: @book.id)
         favorite.save
-        redirect_to book_path(book)
+        redirect_to book_path(@book)
     end
 
     def destroy
